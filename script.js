294226e8c15e3f8c04a8135c71dcac0dd6ad942b
Siva_Sai <22je0483@iitism.ac.in>
2023-12-07 20:21:43 +0530

    Fixed minor bugs

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const gameboard = (function () {
   return { clearBoard, getBoard, selectCell, printBoard };
 })();
 
-const gamecontroller = (function (playerOneName, playerTwoName) {
+const gamecontroller = function (playerOneName, playerTwoName) {
   const players = [
     { name: playerOneName, marker: "X" },
     { name: playerTwoName, marker: "O" },
@@ -63,8 +63,13 @@ const gamecontroller = (function (playerOneName, playerTwoName) {
     else if (row === column) {
       if (board[0][0] === board[1][1] && board[1][1] === board[2][2])
         return true;
-    }else if ((row===0&&column===2) || (row===2&&column===0)||(row===1&&column===1)){
-      
+    } else if (
+      (row === 0 && column === 2) ||
+      (row === 2 && column === 0) ||
+      (row === 1 && column === 1)
+    ) {
+      if (board[0][2] === board[1][1] && board[1][1] === board[2][0])
+        return true;
     }
     return false;
   };
@@ -95,78 +100,89 @@ const gamecontroller = (function (playerOneName, playerTwoName) {
 
   printNewRound();
 
-  return { playRound, getActivePlayer };
-})("Giorno", "Bruno");
+  return { playRound, getActivePlayer, switchPlayer };
+};
 
 const screencontroller = (function () {
-  const containerDiv = document.querySelector(".container");
-  const playerTurnDiv = document.querySelector(".turn");
-  const boardDiv = document.querySelector(".board");
-  const winnerDiv = document.querySelector(".winner");
+  let playerOneName;
+  let playerTwoName;
 
-  const updateScreen = () => {
-    boardDiv.textContent = "";
-
-    const board = gameboard.getBoard();
-    const activePlayer = gamecontroller.getActivePlayer();
-
-    playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
-
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        const cellButton = document.createElement("button");
-        cellButton.classList.add("cell");
-
-        cellButton.dataset.index = [i, j];
-        cellButton.textContent = board[i][j];
-        if (cellButton.textContent === "X") cellButton.classList.add("cell1");
-        else if (cellButton.textContent === "O")
-          cellButton.classList.add("cell2");
-        boardDiv.appendChild(cellButton);
-      }
-    }
+  const startScreen = () => {
+    gameScreen();
   };
 
-  
-  const clickHandlerBoard = (e) => {
-    const selectedCell = e.target.dataset.index;
-    
-    if (!selectedCell) return;
+  const gameController = gamecontroller("Giorno", "Bruno");
 
-    let a = gamecontroller.playRound(
-      parseInt(selectedCell[0]),
-      parseInt(selectedCell[2])
+  const gameScreen = () => {
+    const containerDiv = document.querySelector(".container");
+    const playerTurnDiv = document.querySelector(".turn");
+    const boardDiv = document.querySelector(".board");
+    const winnerDiv = document.querySelector(".winner");
+
+    const updateScreen = () => {
+      boardDiv.textContent = "";
+
+      const board = gameboard.getBoard();
+      const activePlayer = gameController.getActivePlayer();
+
+      playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          const cellButton = document.createElement("button");
+          cellButton.classList.add("cell");
+
+          cellButton.dataset.index = [i, j];
+          cellButton.textContent = board[i][j];
+          if (cellButton.textContent === "X") cellButton.classList.add("cell1");
+          else if (cellButton.textContent === "O")
+            cellButton.classList.add("cell2");
+          boardDiv.appendChild(cellButton);
+        }
+      }
+    };
+
+    const clickHandlerBoard = (e) => {
+      const selectedCell = e.target.dataset.index;
+
+      if (!selectedCell) return;
+
+      let a = gameController.playRound(
+        parseInt(selectedCell[0]),
+        parseInt(selectedCell[2])
       );
       updateScreen();
-      
-      if (a === 1) {
-        winnerDiv.textContent = `${gamecontroller.getActivePlayer().name} won.`;
-        boardDiv.removeEventListener("click", clickHandlerBoard);
-      playAgain();
-    } else if (a === 2) {
-      winnerDiv.textContent = "Its a draw.";
-      boardDiv.removeEventListener("click", clickHandlerBoard);
-      playAgain();
-    }
-  };
-  
-  const clickHandlerPlayAgain = () => {
-    gameboard.clearBoard();
-    updateScreen();
-    winnerDiv.textContent="";
-    boardDiv.addEventListener("click", clickHandlerBoard);
-    const playAgainBtn = document.querySelector(".playagain");
-    containerDiv.removeChild(playAgainBtn);
-  };
-  
-  const playAgain = () => {
-    const playAgainBtn = document.createElement("button");
-    playAgainBtn.classList.add("playagain");
-    playAgainBtn.textContent = "Play Again";
-    containerDiv.append(playAgainBtn);
-    playAgainBtn.addEventListener("click", clickHandlerPlayAgain);
-  };
 
-  boardDiv.addEventListener("click", clickHandlerBoard);
-  updateScreen();
+      if (a === 1 || a == 2) {
+        winnerDiv.textContent =
+          a === 1
+            ? `${gameController.getActivePlayer().name} won.`
+            : "Its a draw.";
+        boardDiv.removeEventListener("click", clickHandlerBoard);
+        playAgain();
+      }
+    };
+
+    const clickHandlerPlayAgain = () => {
+      gameboard.clearBoard();
+      updateScreen();
+      winnerDiv.textContent = "";
+      boardDiv.addEventListener("click", clickHandlerBoard);
+      const playAgainBtn = document.querySelector(".playagain");
+      containerDiv.removeChild(playAgainBtn);
+    };
+
+    const playAgain = () => {
+      const playAgainBtn = document.createElement("button");
+      playAgainBtn.classList.add("playagain");
+      playAgainBtn.textContent = "Play Again";
+      containerDiv.append(playAgainBtn);
+      playAgainBtn.addEventListener("click", clickHandlerPlayAgain);
+      gameController.switchPlayer();
+    };
+
+    boardDiv.addEventListener("click", clickHandlerBoard);
+    updateScreen();
+  };
+  startScreen();
 })();
